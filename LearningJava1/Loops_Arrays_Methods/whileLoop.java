d54fb8264530d6b847472bc515c9038f41b4e863
lhalcomb <lhalcomb@drury.edu>
2023-06-16 13:54:17 -0500

    init

--- LearningJava1/Loops_Arrays_Methods/whileLoop.java
+++ LearningJava1/Loops_Arrays_Methods/whileLoop.java
@@ -2,6 +2,9 @@ package LearningJava1.Loops_Arrays_Methods;
 
 public class whileLoop {
     public static void main(String[] args){
-        
+        int a = 0;
+        while(a >100){
+            System.out.println(a);
+        }
     }
 }
