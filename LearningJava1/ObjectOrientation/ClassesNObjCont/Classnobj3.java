81effb994cd3d39c668824607a2c70d8c6656fcc
lhalcomb <123402670+lhalcomb@users.noreply.github.com>
2023-07-15 22:11:06 -0500

    cm

---
+++ LearningJava1/ObjectOrientation/ClassesNObjCont/Classnobj3.java
@@ -0,0 +1,12 @@
+package LearningJava1.ObjectOrientation.ClassesNObjCont;
+
+public class Classnobj3 {
+    
+  
+    public static void main(String[] args) {
+        spider r = new spider(8,8);
+
+        System.out.println(r.NumOfEyes);
+        System.out.println(r.NumOfLegs);
+     }
+}
