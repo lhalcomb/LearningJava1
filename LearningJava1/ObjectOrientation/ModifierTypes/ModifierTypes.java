6a1b2684c48d1d94c924c70a1d326c160a331756
lhalcomb <lhalcomb@drury.edu>
2023-07-18 13:36:49 -0500

    no

---
+++ LearningJava1/ObjectOrientation/ModifierTypes/ModifierTypes.java
@@ -0,0 +1,14 @@
+package LearningJava1.ObjectOrientation.ModifierTypes;
+
+public class ModifierTypes {
+    public static void main(String[] args){
+       
+        /* Private - can only use objects in the file that are inside of the class
+           Default - this variable or method can be accessed anywhere in the package/root folder
+           Protected - if there is a protected attribute in that class, the class will only be visible in the package unless extended
+           Public - the most open modifier. This modifier makes each file visible to another regardless of package/file
+           Static - a given variable, method, or function is directly related to a class
+           Final - create a constant instead of a variable, can be used on any data types
+           */
+    }
+}
---
+++ LearningJava1/OwnPractice/LinkedList.java
@@ -0,0 +1,169 @@
+package LearningJava1.OwnPractice;
+import java.io.*;
+
+public class LinkedList 
+{
+    Node head;
+   
+    static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int d)
+        {
+            data = d; 
+            next = null;
+        }
+    }
+
+    
+    public static LinkedList insert(LinkedList list, int data)
+    {
+        Node new_node = new Node(data); //<- why the hell isnt this working?, '*static class Node*'
+
+        // If the linkedlist is empty then make this node as head of the Linked List and return
+        if (list.head == null)
+        {
+            list.head = new_node;
+        }
+        //Traverse through the list and add the inserted data at the end 
+        else{
+            Node last = list.head;
+            while (last.next != null){
+                last = last.next; 
+            }
+            last.next = new_node; 
+        }
+        // Return the new inserted list 
+        return list;
+    }
+
+
+    public static LinkedList InsertAfter(LinkedList list, Node prev_node, int new_data)
+    {
+        Node new_node = new Node(new_data);
+
+        if (prev_node == null)
+        {
+            System.out.println("The given previous node cannot be null/empty. ");
+            return list;
+        }
+        while (list.head != null)
+        {
+            new_node.next = prev_node.next;
+            prev_node.next = new_node;
+        }
+    return list;
+       
+
+    }
+
+    public static LinkedList 
+    RemoveNodebyKey(LinkedList list, int key)
+    {
+        Node CurrNode = list.head, prev = null;
+
+        if (CurrNode != null && CurrNode.data == key)
+        {
+            list.head = CurrNode.next;
+            System.out.println(key + "found and deleted");
+            
+            return list;
+
+        }
+
+        while (CurrNode != null && CurrNode.data != key)
+        {
+            prev = CurrNode;
+            CurrNode = CurrNode.next;
+
+        }
+        if (CurrNode != null)
+        {
+            prev.next = CurrNode.next;
+            System.out.println(key + " found and deleted");
+            
+            return list;
+        }
+
+        if (CurrNode == null)
+        {
+            System.out.println("Not Found");
+        
+        }
+        return list;
+    
+    }
+
+    public static LinkedList 
+    DeleteNodeAtPos(LinkedList list, int index)
+    {
+        Node CurrNode = list.head, prev = null;
+
+        if (index == 0 && CurrNode != null) // Case 1: If index is 0, then the head must be deleted
+        {   
+            list.head = CurrNode.next;
+            System.out.println(index + " found at this position");
+            
+            return list;
+        }
+
+        int counter = 0; //Case 2: If the index is greater than 0 but less than the size of LinkedList
+        while (CurrNode != null)
+        {
+            if (counter == index)
+            {
+                prev.next = CurrNode.next;
+
+                System.out.println(index + " position element deleted");
+
+                break;
+
+            }else
+            {
+                prev = CurrNode.next;
+                CurrNode = CurrNode.next;
+                counter++;
+
+            }
+        }
+
+        if (CurrNode == null) //Case 3: The index given is greater than the size of the list
+        {
+            System.out.println(index + " was not found"); //here we will just return a dummy statement 
+        }
+
+        return list;
+
+    }
+
+    public static void printList(LinkedList list)
+    { 
+     Node CurrNode = list.head;
+     System.out.print("Linked List: ");
+      
+     while (CurrNode != null)
+      {
+        System.out.println(CurrNode.data + ' ');
+        CurrNode = CurrNode.next;
+      }
+      System.out.println();
+    }
+
+    public static void main(String[] args){
+
+    LinkedList list = new LinkedList();
+        
+    list = insert(list,1);
+    list = insert(list,2);
+    list = insert(list,3);
+    printList(list);
+
+    RemoveNodebyKey(list,3);
+
+    printList(list);
+
+    }
+}
+
