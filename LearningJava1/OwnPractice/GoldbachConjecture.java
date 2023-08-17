dae5f6c873f79216a0a5adcb7949537b786ccd4b
lhalcomb <lhalcomb@drury.edu>
2023-08-17 13:48:18 -0500

    Gb

--- LearningJava1/OwnPractice/GoldbachConjecture.java
+++ LearningJava1/OwnPractice/GoldbachConjecture.java
@@ -7,14 +7,16 @@ public class GoldbachConjecture {
 
     //Driver Code to run program
     public static void main(String[] args){ 
-        int num = 52; //Even Number to represent sum of two primes. 
+        int num = (int) ((Math.random() * 100)); //Even Number to represent sum of two primes. 
         boolean check = false; //Boolean for checking if the number is even.
+        int counter = 0;
         for (int i = 2; i <= num / 2; ++i){ // For loop that begins the process of seperating the even number into sum of two primes. 
             if (checkPrime(i)){ //If the number is prime it runs down to the function below 
 
                 if (checkPrime(num-i)){ 
                     System.out.printf("%d = %d + %d\n", num, i, num - i);
                     check = true;
+                    counter += 1;
                 }
             }
         }
@@ -22,6 +24,7 @@ public class GoldbachConjecture {
         if (!check){
             System.out.println(num + " cannot be represented as the sum of two prime numbers. ");
         }
+        System.out.printf("The number of equations for %d is %d. ", num, counter);
     }
 
     static boolean checkPrime(int n){ // Functiomn created to check if number is prime and returns that number via driver code for loop. 
