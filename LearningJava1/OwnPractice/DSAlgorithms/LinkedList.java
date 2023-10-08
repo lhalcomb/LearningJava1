61b23a6116096a5760b014dc16e2fb275d99f335
lhalcomb <lhalcomb@drury.edu>
2023-10-08 00:17:25 -0500

    updates

--- LearningJava1/OwnPractice/DSAlgorithms/LinkedList.java
+++ LearningJava1/OwnPractice/DSAlgorithms/LinkedList.java
@@ -144,6 +144,91 @@ public class LinkedList
 
     }
 
+    Node merge(Node a, Node b)
+    {
+        Node result = null;
+        if (a == null)
+        {
+            return a;
+        }
+        if (b == null)
+        {
+            return b;
+        }
+
+        if (a.data <= b.data)
+        {
+            result = a;
+            result.next = merge(a.next, b);
+        }
+        else{
+            result = b; 
+            result.next = merge(b, b.next);
+        }
+        return result;
+    }
+
+    public static Node getMiddle(Node head)
+    {
+        if (head == null)
+        {
+            return head;
+        }
+
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+
+    }
+
+    Node mergeSort(Node h)
+    {
+        if (h == null || h.next == null)
+        {
+            return h;
+        }
+        Node middle = getMiddle(h);
+        Node next2middle = middle.next;
+
+        middle.next = null;
+
+        Node left = mergeSort(h);
+        Node right =  mergeSort(next2middle);
+        Node sortedlist = sortedMerge(left, right);
+
+        return sortedlist;
+    }
+
+    Node sortedMerge(Node a, Node b)
+    {
+        Node result = null;
+
+        if (a == null)
+        {
+            return b;
+        }
+        if (b == null)
+        {
+            return a;
+        }
+
+        if (a.data >= b.data)
+        {
+            result = a;
+            result.next = sortedMerge(a.next, b);
+        }
+        else
+        {
+            result = b;
+            result.next = sortedMerge(a, b.next);
+        }
+        return result;
+    }
 
     public static void printList(LinkedList list)
     { 
@@ -156,6 +241,11 @@ public class LinkedList
         CurrNode = CurrNode.next;
       }
       System.out.println();
+    }
+
+    public static void randomList(LinkedList list)
+    {
+        ;
     }
     
 
--- LearningJava1/OwnPractice/DSAlgorithms/SortAlgorithm.java
+++ LearningJava1/OwnPractice/DSAlgorithms/SortAlgorithm.java
@@ -2,7 +2,8 @@ package LearningJava1.OwnPractice.DSAlgorithms;
 
 import java.util.Random;
 
-public class SortAlgorithm {
+public class SortAlgorithm 
+{
 
     static void bubblesort(int arr[], int n){
         int temp, i, j;
@@ -23,11 +24,7 @@ public class SortAlgorithm {
                     break;
                 }
         }
-    }
-    static void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp; 
+    
     }
 
     static int partition(int[] arr, int low, int high)
@@ -36,22 +33,36 @@ public class SortAlgorithm {
         int pivot = arr[high];
         int i = (low - 1);
 
-        for (int j = low; i <= high - 1; i++)
+        for (int j = low; j <= high - 1; j++)
         {
-            if (arr[j] < pivot) 
+            if (arr[j] <= pivot) 
             {
                 i++;
-                swap(arr, i, j);
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
 
         }
-        swap(arr, i + 1, high);
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
         return (i + 1);
 
     }
     
 
-    static void quickSort(int arr[], )
+    static void quickSort(int arr[], int low, int high)
+        {
+        if (low < high) 
+            {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+            }
+        }
 
 
 
@@ -67,20 +78,35 @@ public class SortAlgorithm {
         public static void main(String args[]){
 
             
-            Random rand = new Random();
-            int[] arr = new int[1];
+            Random randi = new Random();
+            int arr[] = new int[1];
             for (int i = 0; i < arr.length; i++){
-                arr[i] = rand.nextInt(100)+1;
+                arr[i] = randi.nextInt(10)+1;
         } 
+
             int n = arr.length;
             final long startBS = System.nanoTime();
             bubblesort(arr, n); 
             final long endBS = System.nanoTime();
             long elapsedBS = endBS - startBS;
-            double convertBS = (double) elapsedBS / 1000000000;
             System.out.println("The sorted list is: ");
             printarray(arr, n);
-            System.out.println("Total Bubble Sort execution time: " + (convertBS) + "s\n");
+            System.out.println("Total Bubble Sort execution time: " + (elapsedBS) + "ms\n");
+
+            Random randj = new Random();
+            int arrj[] = new int[1];
+            for (int j = 0; j < arrj.length; j++){
+                arrj[j] = randj.nextInt(10)+1;
+        } 
+
+            int N = arrj.length;
+            final long startQS = System.nanoTime();
+            quickSort(arrj, 0, N-1); 
+            final long endQS = System.nanoTime();
+            long elapsedQS = endQS - startQS;
+            System.out.println("The sorted list is: ");
+            printarray(arr, N);
+            System.out.println("Total Quick Sort execution time: " + (elapsedQS) + "ms\n");
 
     
 
