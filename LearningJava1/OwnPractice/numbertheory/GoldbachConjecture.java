7b711d27a76f646058d77f59471a47008d349e08
lhalcomb <lhalcomb@drury.edu>
2024-02-26 23:05:34 -0600

    Implemented Prime Intensity Checker

--- LearningJava1/OwnPractice/numbertheory/GoldbachConjecture.java
+++ LearningJava1/OwnPractice/numbertheory/GoldbachConjecture.java
@@ -13,19 +13,30 @@ public class GoldbachConjecture {
 
     //Driver Code to run program
     public static void main(String[] args){ 
-        //int n = (int) ((Math.random() * 100)); //Even Number to represent sum of two primes. 
-        int n = 50; // Number of even numbers to generate and check
+
+
+        int n = (int) ((Math.random() * 100)); //Even Number to represent sum of two primes. 
+        //int n = 50; // Number of even numbers to generate and check
+
+        System.out.printf("Printing" + n + " amount of equations");
         Map<Integer, Integer> EvenNumberofEq = new HashMap<>();
+        Map<Integer, Integer> PrimeIntensityMap = new HashMap<>();
+
         List<Integer> EqCountList = new ArrayList<>();
+
+
+
         for (int i = 2; i <= n; i++) {
             int num = 2 * i; // Generating even numbers
             //System.out.println("Even number: " + num);
 
             int equationCount = SumSeperation(num);
             EvenNumberofEq.put(num, equationCount);
-
+            updatePrimeIntensityMap(num, PrimeIntensityMap);
             System.out.println(); // Add a newline for better readability
         }
+
+
         // Print the map
         System.out.println("Even numbers and their corresponding equations count:");
         for (Map.Entry<Integer, Integer> entry : EvenNumberofEq.entrySet()) {
@@ -34,12 +45,34 @@ public class GoldbachConjecture {
             System.out.println("Even number: " + evenNumber + ", Equations count: " + equationsCount);
             EqCountList.add(equationsCount);
         }
-        
-        for (int count: EqCountList){
-            System.out.print(count+", ");
+
+
+        //Print Prime Intensity Analysis
+        for (Map.Entry<Integer, Integer> entry : PrimeIntensityMap.entrySet()){
+            int primeNumber = entry.getKey();
+            int intensity = entry.getValue();
+            if (checkPrime(primeNumber)){
+                System.out.println("Prime number: " + primeNumber + ", Intensity: " + intensity);
+            }
         }
         
+        // for (int count: EqCountList){
+        //     System.out.print(count+", ");
+        // }
+        
     }
+
+
+
+    static void updatePrimeIntensityMap(int num, Map<Integer, Integer> PrimeIntensityMap){
+        for (int i = 2; i <= num/ 2; i++){
+            if (checkPrime(i) && checkPrime( - i)){
+                PrimeIntensityMap.put(i, PrimeIntensityMap.getOrDefault(i, 0) + 1);
+                PrimeIntensityMap.put(num - i, PrimeIntensityMap.getOrDefault(num - i, 0) + 1);
+            }
+        }
+    }
+
 
 
     static int SumSeperation(int num){
@@ -65,6 +98,8 @@ public class GoldbachConjecture {
         return counter;
     }
 
+
+
     static boolean checkPrime(int n){ // Function created to check if number is prime and returns that number via driver code for loop. 
         boolean isPrime = true;
 
@@ -76,4 +111,7 @@ public class GoldbachConjecture {
         }
         return isPrime; 
     }
+
+
+
 }
